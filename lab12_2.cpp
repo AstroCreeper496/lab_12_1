529fbe6103e72da2cac3fb35c59c89a49f1da26d
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-01-06 12:21:53 +0700

    Update lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+long long int gcd(long long int a, long long int b);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+long long int gcd(long long int a, long long int b){
+    if (a % b == 0) return b;
+    else return gcd(b, a % b);
 }
