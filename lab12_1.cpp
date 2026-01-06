890f5cf346c57ac5f8f9f8e9593880c7b592ec4b
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-01-06 12:10:22 +0700

    lab12_1.cpp 0.2

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
+#include<ctime>
+#include<cmath>
 using namespace std;
 
-int fibonacci(int x);
+long long int fibonacci(int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int n){
+long long int fibonacci(int n){
     if(n <= 0) return 0;
     if(n == 1) return 1;
     else {
-        int s = fibonacci(n - 1) + fibonacci(n - 2);
+        long long int s = fibonacci(n - 1) + fibonacci(n - 2);
         return s;
     }
 }
